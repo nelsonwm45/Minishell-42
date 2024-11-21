51ac83d35dc3c116a1a325a9dd11266948091bc6
nelsonwm45 <nelsonwm45@gmail.com>
2024-11-22 03:13:16 +0800

    Heredoc now seems working

--- source/heredoc/heredoc.c
+++ source/heredoc/heredoc.c
@@ -3,18 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchok <nchok@student.42kl..edu.my>         +#+  +:+       +#+        */
+/*   By: nchok <nchok@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/14 13:30:57 by nchok             #+#    #+#             */
-/*   Updated: 2024/11/19 17:25:44 by nchok            ###   ########.fr       */
+/*   Created: 2024/11/22 02:11:34 by nchok             #+#    #+#             */
+/*   Updated: 2024/11/22 02:11:34 by nchok            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "../../header/minishell.h"
+#include "../../header/minishell.h"
 
 int	start_heredoc(t_general *utils, t_cmds *cmds)
 {
-	t_lexer		*ptr;
+	t_lexer	*ptr;
 	int		status;
 
 	ptr = cmds->redir;
@@ -25,18 +25,16 @@ int	start_heredoc(t_general *utils, t_cmds *cmds)
 		{
 			if (cmds->hd_file_name)
 				free(cmds->hd_file_name);
-			cmds->hd_file_name = create_hd_filename();
+			cmds->hd_file_name = create_hd_filename(utils);
 			status = mini_heredoc(utils, ptr, cmds->hd_file_name);
 			if (status != EXIT_SUCCESS)
 			{
 				utils->exit_status = 1;
 				return (clean_utils(utils));
 			}
-			
 		}
 		ptr = ptr->next;
 	}
-	ptr = cmds->redir; // probably not needed
 	return (EXIT_SUCCESS);
 }
 
@@ -46,19 +44,46 @@ int	start_heredoc(t_general *utils, t_cmds *cmds)
 	- filename will be in the format of /tmp/.minishell_hd_{index}
 	- index will be incremented each time this function is called
 */
-char	*create_hd_filename(void)
+char	*create_hd_filename(t_general *utils)
 {
 	static int	i = 0;
-	char	*filename;
-	char	*file_index;
+	char		*filename;
+	char		*file_index;
+	char		*pwd;
+	char		*tmp;
 
 	file_index = ft_itoa(i);
-	if (!file_index)
-		return (NULL);
 	i++;
-	filename = ft_strjoin("/tmp/.minishell_hd_", file_index);
+	pwd = utils->pwd;
+	printf("pwd: %s\n", pwd);
+	pwd = remove_pwd(pwd);
+	tmp = ft_strjoin(pwd, "/.minishell_hd_");
+	printf("tmp: %s\n", tmp);
+	filename = ft_strjoin(tmp, file_index);
+	printf("filename: %s\n", filename);
+	free(tmp);
 	free(file_index);
 	return (filename);
+}
+
+char	*remove_pwd(char *pwd)
+{
+	int			i;
+	int			j;
+	const char	*str;
+
+	i = 0;
+	j = 0;
+	str = "PWD=";
+	while (pwd[i] && str[j])
+	{
+		if (pwd[i] == str[j])
+		{
+			i++;
+			j++;
+		}
+	}
+	return (pwd + j);
 }
 
 /*
@@ -72,13 +97,12 @@ char	*create_hd_filename(void)
 */
 int	mini_heredoc(t_general *utils, t_lexer *ptr, char *filename)
 {
-	int		status;
-	int		have_quote;
-	pid_t	pid;
+	int	status;
+	int	have_quote;
 
-	(void)filename;
 	status = EXIT_SUCCESS;
-	if ((ptr->str[0] == '\'' && ptr->str[ft_strlen(ptr->str) - 1] == '\'') || (ptr->str[0] == '\"' && ptr->str[ft_strlen(ptr->str) - 1] == '\"'))
+	if ((ptr->str[0] == '\'' && ptr->str[ft_strlen(ptr->str) - 1] == '\'')
+		|| (ptr->str[0] == '\"' && ptr->str[ft_strlen(ptr->str) - 1] == '\"'))
 		have_quote = 1;
 	else
 		have_quote = 0;
@@ -86,11 +110,8 @@ int	mini_heredoc(t_general *utils, t_lexer *ptr, char *filename)
 	remove_quotes(ptr->str, '\"');
 	utils->stop_heredoc = 0;
 	utils->in_heredoc = 1;
-	pid = fork();
-	if (pid == 0)
-		status = create_heredoc(utils, ptr, filename, have_quote);
+	create_heredoc(utils, ptr, filename, have_quote);
 	// signal(SIGQUIT, SIG_IGN);
-	waitpid(pid, &status, 0);
 	utils->in_heredoc = 0;
 	utils->heredoc = 1;
 	return (status);
@@ -103,28 +124,31 @@ int	mini_heredoc(t_general *utils, t_lexer *ptr, char *filename)
 	- write the input to the file
 	- close the file
 */
-int	create_heredoc(t_general *utils, t_lexer *ptr, char *filename, int have_quote)
+int	create_heredoc(t_general *utils, t_lexer *ptr, char *filename,
+		int have_quote)
 {
-	int	pipe_fd[2];
+	int		fd;
 	char	*line;
+	int		cmp;
 
-	(void)filename;
-	if (pipe(pipe_fd) == -1)
-		return (error_message(4, utils));
-	// signal(SIGINT, SIG_DFL);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0644);
+	printf("filename: %s\n", filename);
+	print_redir(ptr);
 	line = readline("\033[0;32mHeredoc> \033[0m");
-	while (line && ft_strncmp(ptr->str, line, ft_strlen(ptr->str)) == 0 && utils->stop_heredoc != 1)
+	while (line && ft_strncmp(ptr->str, line, ft_strlen(ptr->str)) != 0
+		&& utils->stop_heredoc != 1)
 	{
-		if (have_quote == 1)
+		cmp = ft_strncmp(ptr->str, line, ft_strlen(ptr->str));
+		printf("cmp: %d\n", cmp);
+		if (have_quote == 0)
 			line = expand_str(utils, line);
-		ft_putendl_fd(line, pipe_fd[1]);
+		ft_putendl_fd(line, fd);
 		free(line);
 		line = readline("\033[0;32mHeredoc> \033[0m");
 	}
 	free(line);
 	if (utils->stop_heredoc == 1 || !line)
 		return (EXIT_FAILURE);
-	close(pipe_fd[1]);
-	close(pipe_fd[0]);
+	close(fd);
 	return (EXIT_SUCCESS);
 }
--- source/start_shell.c
+++ source/start_shell.c
@@ -108,6 +108,7 @@ int	start_shell(t_general *utils)
 		setup_executor(utils);
 		// Restore signals for the main process after execution
 		run_signals(1);
+		clean_utils(utils);
 
 		token = malloc(sizeof(t_token));
 		if (!token)
