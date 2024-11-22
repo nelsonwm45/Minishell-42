63b8d2f5e011081607273f35c2c7271237c31203
winniehhy <yoyowinnie3@gmail.com>
2024-11-22 09:44:32 +0800

    rename

--- source/builtin/builtin_cd.c
+++ source/builtin/builtin_cd.c
@@ -16,7 +16,9 @@ static void print_error(char **args)
 {
     ft_putstr_fd("cd: ", 2);
     if (args[2])
+    {
         ft_putstr_fd("string not in pwd: ", 2);
+    }
     else
     {
         ft_putstr_fd(strerror(errno), 2);
