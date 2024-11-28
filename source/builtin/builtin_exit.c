2a91f71c6babc04fb981b759120f9ff5d398d331
winniehhy <yoyowinnie3@gmail.com>
2024-11-29 04:23:55 +0800

    exit ok

--- source/builtin/builtin_exit.c
+++ source/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 13:45:00 by hheng             #+#    #+#             */
-/*   Updated: 2024/10/30 13:45:00 by hheng            ###   ########.fr       */
+/*   Updated: 2024/11/29 04:23:36 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,38 @@
 	-Invalid arguments (e.g., exit abc). 
 */
 
+static void	print_exit_message(char **cmd)
+{
+	ft_putstr_fd("exit ", 2);
+	if (cmd[1])
+		ft_putendl_fd("BYE ~ ❤", 2);
+	else
+		ft_putendl_fd("💚", 2);
+}
+
 void	ft_exit(t_shell *mini, char **cmd)
 {
-	mini->exit_code= 1;
-	ft_putstr_fd("exit ", 2);
-	cmd[1] ? ft_putendl_fd("BYE ~ ❤", 2) : ft_putendl_fd("💚", 2);
+	mini->exit_code = 1;
+	print_exit_message(cmd);
 	if (cmd[1] && cmd[2])
 	{
 		mini->return_code = 1;
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
+		return ;
 	}
-	else if (cmd[1] && ft_strisnum(cmd[1]) == 0)
+	if (cmd[1])
 	{
-		mini->return_code = 255;
-		ft_putstr_fd("minishell: exit: ", 2);
-		ft_putstr_fd(cmd[1], 2);
-		ft_putendl_fd(": numeric argument required", 2);
-	}
-	else if (cmd[1])
+		if (ft_strisnum(cmd[1]) == 0)
+		{
+			mini->return_code = 255;
+			ft_putstr_fd("minishell: exit: ", 2);
+			ft_putstr_fd(cmd[1], 2);
+			ft_putendl_fd(": numeric argument required", 2);
+			exit(mini->return_code);
+		}
 		mini->return_code = ft_atoi(cmd[1]);
+	}
 	else
 		mini->return_code = 0;
-
-	// printf("Exiting minishell...\n");
-    // free_tab(cmd);
-     exit(mini->return_code);
+	exit(mini->return_code);
 }
